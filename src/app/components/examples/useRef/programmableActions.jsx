e50f1eb418e95ad5a32322313b642d121a9a243b
FunChosa <gilazieva.00@mail.ru>
2023-04-14 18:59:39 +0300

    1859

---
+++ src/app/components/examples/useRef/programmableActions.jsx
@@ -0,0 +1,42 @@
+import React, { useRef } from "react";
+import CardWrapper from "../../common/Card";
+import SmallTitle from "../../common/typografy/smallTitle";
+const ProgrammableActionsExample = () => {
+    const inputRef = useRef();
+    const handleClick = () => {
+        console.log(inputRef.current);
+        inputRef.current.focus();
+    };
+
+    const handleClickWidth = () => {
+        inputRef.current.style.width = "100px";
+    };
+
+    return (
+        <CardWrapper>
+            <SmallTitle className="card-title">
+                Программируемые действия и свойства
+            </SmallTitle>
+            <label htmlFor="email" className="form-label">
+                Email
+            </label>
+            <input
+                ref={inputRef}
+                type="email"
+                className="form-control"
+                id="email"
+            />
+            <button className="btn btn-primary mt-2" onClick={handleClick}>
+                focus input
+            </button>
+            <button
+                className="btn btn-secondary mt-2 mx-2"
+                onClick={handleClickWidth}
+            >
+                изменить ширину объекта
+            </button>
+        </CardWrapper>
+    );
+};
+
+export default ProgrammableActionsExample;
---
+++ src/app/components/exercises/useRefExercise.jsx
@@ -0,0 +1,44 @@
+import React, { useRef } from "react";
+import CollapseWrapper from "../common/collapse";
+const UseRefExercise = () => {
+    const blockRef = useRef("");
+
+    const handleClickChange = () => {
+        blockRef.current.style.width = "150px";
+        blockRef.current.style.height = "80px";
+        blockRef.current.textContent = "TEXT";
+    };
+    return (
+        <CollapseWrapper title="Упражнение">
+            <p className="mt-3">
+                У вас есть блок, у которого заданы ширина и высота. Добавьте
+                кнопку, при нажатии которой изменятся следующие свойства:
+            </p>
+            <ul>
+                <li>Изменится содержимое блока на &quot;text&quot;</li>
+                <li>высота и ширина станут равны 150 и 80 соответственно</li>
+            </ul>
+
+            <div
+                className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
+                ref={blockRef}
+                style={{
+                    height: 40,
+                    width: 60,
+                    color: "white"
+                }}
+            >
+                <small>Блок</small>
+            </div>
+
+            <button
+                className="btn btn-success mt-2"
+                onClick={handleClickChange}
+            >
+                Update Block
+            </button>
+        </CollapseWrapper>
+    );
+};
+
+export default UseRefExercise;
